d6de1d4fd2dd2a22f356a6953fd6fd9fd339a595
bened18 <belt@eafit.edu.co>
2023-02-09 12:12:39 -0500

    pagina de about creada

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page from Benjamin</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Benjamin Eduardo De La Torre Rojas'})
+
+def about(request):
+    return render(request, 'about.html', {'name':'Benjamin De La Torre'})
